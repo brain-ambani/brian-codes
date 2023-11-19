a288c93a64e1bc2be72681b884c9bf0b3d8d8d95
brian-ambani <brainambani1@gmail.com>
2023-11-19 14:10:53 +0300

    updated the compontent wrapper

--- src/components/ComponentWrapper.jsx
+++ src/components/ComponentWrapper.jsx
@@ -3,11 +3,13 @@ import Nav from "./nav/Nav";
 import About from "./about/About";
 import Work from "./work/Work";
 import Contact from "./contact/Contact";
+import Hero from "./hero/Hero";
 
 export default function ComponentWrapper() {
   return (
     <div>
       <Nav />
+      <Hero />
       <About />
       <Work />
       <Contact />
