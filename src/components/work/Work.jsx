ee21473e301e47c73f54028fed9ab493132513ea
brian-ambani <brainambani1@gmail.com>
2023-12-25 04:30:18 +0300

    updated the hulu project status as completed

--- src/components/work/Work.jsx
+++ src/components/work/Work.jsx
@@ -70,29 +70,6 @@ export default function Work() {
           </div>
         </div>
         <div class="project-card tracker-card">
-          <img src={workoutbuddy} width={300} height={200} alt="project 1" />
-          <h3>Workout Buddy</h3>
-          <p>
-            MERN stack application for tracking workouts and fitness progress.
-          </p>
-          <a href="#work" className=" btn-work">
-            Live Preview
-          </a>
-          <a
-            href="https://github.com/brian-ambani/workout-buddy/"
-            target="_blank"
-            rel="noreferrer"
-            className=" btn-work"
-          >
-            Source Code
-          </a>
-          <div className="status">
-            <p>
-              Status: <span>Ongoing</span>
-            </p>
-          </div>
-        </div>
-        <div class="project-card tracker-card">
           <img src={hulu} width={300} height={200} alt="project thumbnail" />
           <h3>Hulu App</h3>
           <p>
@@ -107,6 +84,29 @@ export default function Work() {
           </a>
           <a
             href="https://github.com/brian-ambani/hulu-clone"
+            target="_blank"
+            rel="noreferrer"
+            className=" btn-work"
+          >
+            Source Code
+          </a>
+          <div className="status">
+            <p>
+              Status: <span>Completed</span>
+            </p>
+          </div>
+        </div>
+        <div class="project-card tracker-card">
+          <img src={workoutbuddy} width={300} height={200} alt="project 1" />
+          <h3>Workout Buddy</h3>
+          <p>
+            MERN stack application for tracking workouts and fitness progress.
+          </p>
+          <a href="#work" className=" btn-work">
+            Live Preview
+          </a>
+          <a
+            href="https://github.com/brian-ambani/workout-buddy/"
             target="_blank"
             rel="noreferrer"
             className=" btn-work"
